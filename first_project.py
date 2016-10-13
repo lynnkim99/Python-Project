4e39aeb052fe7e2093257852ebae4211bb0767db
Lynn Kim <lkim2018@chadwickschool.org>
2016-10-12 23:48:17 -0700

    square

---
+++ first_project.py
@@ -0,0 +1,12 @@
+from Myro import *
+init("sim") #if simulator is not running
+penDown()
+forward(2,1)
+turnBy(90)
+forward(2,1)
+turnBy(90)
+forward(2,1)
+turnBy(90)
+forward(2,1)
+#
+python> askQuestion("Number of sides?")
